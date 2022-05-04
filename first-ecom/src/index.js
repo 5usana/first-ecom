fb0c5a858566fcc40fbf62bf5d7d0672814f2fc0
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-05-04 09:52:09 -0700

    started CreatorProfile with links to Github account & LinkedIn

--- first-ecom/src/index.js
+++ first-ecom/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CreatorProfile from './components/CreatorProfile';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ ReactDOM.render(
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<App />}></Route>
+				<Route path='/CreatorProfile' element={<CreatorProfile />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>,
